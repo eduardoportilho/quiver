399ecb7fa76afb7d5f60ff85658c2d7f8e02c38e
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-06-15 09:41:57 +0200

    Document next steps

--- index.js
+++ index.js
@@ -9,10 +9,26 @@ var commands = [
 
 term.bold.green('Type part of your command: ');
 
-// - User types (any key but the ones bellow):
-//    - echo the typing
-//    - display the list of filtered commands
-// - UP, DOWN:
-//    - Highlight item on the filtered command list
-// - ENTER:
-//    - Execute highlighted command
+/***************************************************
+ * Desired behaviour:
+ *  - User types (any key but the ones bellow):
+ *    - echo the typing
+ *    - display the list of filtered commands
+ * - UP, DOWN:
+ *    - Highlight item on the filtered command list
+ * - ENTER:
+ *    - Execute highlighted command
+ *    
+ * Aproaches:
+ *  1. inputField
+ *     - Implement much of the desired behaviour (with autocomplete)
+ *     - Maybe a bit inflexible on the UX:
+ *         - TAB for cicling trough the options
+ *         
+ *  2. grabInput
+ *     - Lower level, will require a lot of code
+ * 
+ * Next steps:
+ *  1. Implemt prototype with autocomplete
+ *  
+ ***************************************************/
